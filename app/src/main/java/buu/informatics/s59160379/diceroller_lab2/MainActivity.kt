e5045c352144702d5a5e973e684dda5f12390506
xxxDNxxx <beerurban@gmail.com>
2019-08-21 00:54:22 +0700

    Commit 2 Toast

--- app/src/main/java/buu/informatics/s59160379/diceroller_lab2/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160379/diceroller_lab2/MainActivity.kt
@@ -3,6 +3,8 @@ package buu.informatics.s59160379.diceroller_lab2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val RollButton: Button = findViewById(R.id.roll_button)
+        roll_button.setOnClickListener{rollDice()}
+    }
+
+    private fun rollDice() {
+        Toast.makeText(this,"button clicked",Toast.LENGTH_SHORT).show()
     }
 }
