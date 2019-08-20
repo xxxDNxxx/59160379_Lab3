0e8f2b2a42a1080afb3cdf3ee9ed5e90e25b5346
xxxDNxxx <beerurban@gmail.com>
2019-08-21 05:52:23 +0700

    reset button

--- app/src/main/java/buu/informatics/s59160379/diceroller_lab2/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160379/diceroller_lab2/MainActivity.kt
@@ -19,6 +19,13 @@ class MainActivity : AppCompatActivity() {
         val resultText :TextView = findViewById(R.id.result_text)
         resultText.text = "Dice Roll!"
 
+        val resetButton: Button = findViewById(R.id.reset_button)
+        resetButton.setOnClickListener{resetDice()}
+    }
+
+    private fun resetDice() {
+        val resultText : TextView = findViewById(R.id.result_text)
+        resultText.text = "0".toString()
     }
 
     private fun rollDice() {
